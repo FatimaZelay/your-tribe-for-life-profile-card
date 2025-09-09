914af77c0482d41f668fd111df60f7938ecf6c3e
FatimaZelay <fatima.elay@gmail.com>
2025-09-09 15:33:15 +0200

    feat: homepage startup #11 #5

--- profilecard/src/routes/+page.svelte
+++ profilecard/src/routes/+page.svelte
@@ -4,3 +4,27 @@
     const goToAbout = () => {
     console.log("Go to about page");
   };
+  
+</script>
+
+
+  <main class="page">
+   
+  </main>
+  
+  <style>
+    
+    .page {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
+      padding: 2rem;
+      background: #F4E3E8;
+      gap: 2rem;
+    }
+  
+
+  </style>
+  
