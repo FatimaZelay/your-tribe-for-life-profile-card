3e7e19a5f5116597e4c62c6f7ee1b9ad325cbd5c
FatimaZelay <fatima.elay@gmail.com>
2025-09-09 15:30:31 +0200

    feat: created file for "About me" button #8  #7

---
+++ profilecard/src/lib/components/AboutMeButton.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    export let label: string = "About Me";
+    export let type: "button" | "submit" = "button";
+    export let onClick: (() => void) | null = null;
+  </script>
