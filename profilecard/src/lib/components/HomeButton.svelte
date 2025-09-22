2ec68c8f4fa32a234ac2d5abc9d8269efef59727
FatimaZelay <fatima.elay@gmail.com>
2025-09-22 11:35:44 +0200

    fix: import en export homebutton  #5

---
+++ profilecard/src/lib/components/HomeButton.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { goto } from "$app/navigation";
+  
+    export let label = "Home";
+    export let type = "button"; // kan ook "submit" zijn
+  
+    const goHome = () => goto("/");
+  </script>
