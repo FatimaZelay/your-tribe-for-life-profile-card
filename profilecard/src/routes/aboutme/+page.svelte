5f8aa8242c58884f087e28f8d633fba40753c367
FatimaZelay <fatima.elay@gmail.com>
2025-09-22 11:24:42 +0200

    feat: Import HomeButton #13

---
+++ profilecard/src/routes/aboutme/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import HomeButton from '$lib/components/HomeButton.svelte';
+
+    export let data;
+    const member = data.member.data[0]; // Directus response zit in .data[0]
+  </script>
